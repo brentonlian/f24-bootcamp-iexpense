87701609c4f41ade9734e7318a1d8d4fe49b5590
Brenton Lian <73314123+brentonlian@users.noreply.github.com>
2024-10-14 17:49:27 -0400

    Add files via upload

---
+++ f24_iExpense_tempApp.swift
@@ -0,0 +1,17 @@
+//
+//  f24_iExpense_tempApp.swift
+//  f24-iExpense-temp
+//
+//  Created by Brenton on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct f24_iExpense_tempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
